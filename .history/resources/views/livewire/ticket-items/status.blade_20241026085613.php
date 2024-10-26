9c9fa4918fcba3630f48d5da7ddb85e4348b38d1
Viraj Vikum <virajempire2001@gmail.com>
2024-10-26 11:56:49 +0530

    updated with datatables

--- .history/resources/views/livewire/ticket-items/status.blade_20241026085613.php
+++ .history/resources/views/livewire/ticket-items/status.blade_20241026085613.php
@@ -1,6 +1,6 @@
 <div
     class=" @if ($ticketStatusId == 1) bg-green-300 @elseif($ticketStatusId == 2) bg-red-300 @elseif($ticketStatusId == 3) bg-gray-300 @else bg-yellow-200 @endif
-     py-2 px-12 rounded" wire:poll.1500ms>
+     py-2 px-12 rounded" wire:poll.750ms>
     <label class="">
         @if ($ticketStatusId == 1)
             New
